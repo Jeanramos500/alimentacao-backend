979e4ae22719c4e2af41e0c48b19c7cf00e99d28
Jeanramos500 <Jean_2009.ramos@hotmail.com>
2020-05-11 22:19:16 +0000

    Crud

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const server = express();
 server.use(express.json());
 
 const alimentos = [
-        {id:1, nome:'Arroz', quantidade: 2, gramas : 240}, 
-        {id:2, nome: 'Feijao', quantidade: 3, gramas : 2450}]
+        { nome:'Frango', quantidade: 2, gramas : 240}, 
+        { nome: 'Batata', quantidade: 3, gramas : 2450}]
  
 server.get('/alimentacao2', function(request, response) {
     response.json(alimentos);
@@ -14,21 +14,20 @@ server.get('/alimentacao2', function(request, response) {
 
 server.post('/alimentacao2', function(request, response){
     
-    const {id, nome, quantidade,gramas} = request.body;
+    const {nome, quantidade,gramas} = request.body;
 
-    alimentos.push({id,nome, quantidade, gramas});
+    alimentos.push({nome, quantidade, gramas});
     response.status(204).send();
 
 })
 
  server.put('/alimentacao2/:id', function(request,response){
     const id= request.params.id;  
-    const {id,nome, quantidade, gramas} = request.body;
+    const {nome, quantidade, gramas} = request.body;
 
     for(let i = 0 ; i <alimentos.length; i++)
     {
         if(alimentos[i].nome == id ){
-            alimentos[i].id = id;
             alimentos[i].nome = nome;
             alimentos[i].quantidade = quantidade;
             alimentos[i].gramas = gramas;
